11f5a2056aa55ab28dc7f0f3e87462f8c0ec2ceb
Riley-Boynton <160263531+Riley-Boynton@users.noreply.github.com>
2024-03-14 09:57:21 -0700

    added start screen with instructions

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ goals, rules, feedback, freedom, what the verb, and will it form a sentence
 
 battling for the most coins
 same-computer multiplayer
-hidden warp areas
+start and end screen
 '''
 
 # all the things from the other files
@@ -131,6 +131,14 @@ class Game:
         for y in range(0, HEIGHT, TILESIZE):
             pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
 
+    def draw_text(self, surface, text, size, color, x, y):
+        font_name = pg.font.match_font('times new roman')
+        font = pg.font.Font(font_name, size)
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (x,y)
+        surface.blit(text_surface, text_rect)
+
     def draw(self):
         self.screen.fill(BGCOLOR)
         self.draw_grid()
@@ -159,13 +167,30 @@ class Game:
                 #     if event.key == pg.K_DOWN:
                 #         self.player.move(dy=1)
     def show_start_screen(self):
-        pass
-    def show_go_screen(self):
-        pass
+        self.screen.fill(BGCOLOR)
+        self.draw_text(self.screen, "MARIO VS LINK", 96, BLACK, WIDTH/2, 90)
+        self.draw_text(self.screen, "Mario: use WASD to move", 48, RED, WIDTH/2, 210)
+        self.draw_text(self.screen, "Link: use arrows to move", 48, YELLOW, WIDTH/2, 260)
+        self.draw_text(self.screen, "The goal is to collect all the coins the fastest", 36, BLACK, WIDTH/2, 325)
+        self.draw_text(self.screen, "There are seven (7) coins in total", 36, BLACK, WIDTH/2, 360)
+        self.draw_text(self.screen, "PRESS ANY KEY TO BEGIN", 72, BLACK, WIDTH/2, 410)
+        pg.display.flip()
+        self.wait_for_key()
+
+    def wait_for_key(self):
+        waiting = True
+        while waiting:
+            self.clock.tick(FPS)
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    waiting = False
+                    self.quit()
+                if event.type == pg.KEYUP:
+                    waiting = False
 ####################### Instantiate game... ###################
 g = Game()
-# g.show_go_screen()
+g.show_start_screen()
 while True:
     g.new()
     g.run()
-    # g.show_go_screen()
+    g.show_start_screen()
